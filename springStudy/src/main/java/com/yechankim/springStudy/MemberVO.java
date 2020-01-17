22868ab8e27b057744ac509c8174f646e1821811
YechanKim <47955992+dpcks0919@users.noreply.github.com>
2020-01-17 14:32:46 +0900

    controller rerturn type

---
+++ springStudy/src/main/java/com/yechankim/springStudy/MemberVO.java
@@ -0,0 +1,25 @@
+package com.yechankim.springStudy;
+
+public class MemberVO {
+    
+    private String userid;
+    private String userpw;
+    
+    public String getUserid() {
+        return userid;
+    }
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+    public String getUserpw() {
+        return userpw;
+    }
+    public void setUserpw(String userpw) {
+        this.userpw = userpw;
+    }
+ 
+    @Override
+    public String toString(){
+        return "MemberVO [User Id = " + userid + ", User Pw = " + userpw + "]";
+    }
+}
