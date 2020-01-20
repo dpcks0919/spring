57b9126c677ea7b344c4a3b5524936bbdf53c084
YechanKim <47955992+dpcks0919@users.noreply.github.com>
2020-01-20 11:47:17 +0900

    CRUDExample

---
+++ springMVCCRUDEx/src/main/java/com/yechan/crudEx/Emp.java
@@ -0,0 +1,35 @@
+package com.yechan.crudEx;
+
+public class Emp {
+	private int ID;
+	private String NAME;
+	private int SALARY;
+	private String DESIGNATION;
+	
+	public int getId() {
+		return ID;
+	}
+	public void setId(int iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return NAME;
+	}
+	public void setName(String nAME) {
+		NAME = nAME;
+	}
+	public int getSalary() {
+		return SALARY;
+	}
+	public void setSalary(int sALARY) {
+		SALARY = sALARY;
+	}
+	public String getDesignation() {
+		return DESIGNATION;
+	}
+	public void setDesignation(String dESIGNATION) {
+		DESIGNATION = dESIGNATION;
+	}
+
+
+}
